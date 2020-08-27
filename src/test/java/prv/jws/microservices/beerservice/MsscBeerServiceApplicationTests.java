3868d8e4fb38360697a1d924c107206825ccaf07
Jurek <jurek.szymanski@gmail.com>
2020-08-27 21:45:53 +0200

    New project initial release

---
+++ src/test/java/prv/jws/microservices/beerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package prv.jws.microservices.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
