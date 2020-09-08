7291b67af8321f198a3aaeb233a3ce31f1ef0b06
Jurek <jurek.szymanski@gmail.com>
2020-09-08 21:23:43 +0200

    Added simple Controller with get/post/put for BeerDto. Closes #3

--- src/main/java/prv/jws/microservices/beerservice/web/controller/impl/BeerControllerImpl.java
+++ src/main/java/prv/jws/microservices/beerservice/web/controller/impl/BeerControllerImpl.java
@@ -4,7 +4,6 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.HeadersBuilder;
 import org.springframework.web.bind.annotation.RestController;
 import prv.jws.microservices.beerservice.web.controller.BeerController;
 import prv.jws.microservices.beerservice.web.model.BeerDto;
