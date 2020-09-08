e2f48b799001b2c7117467f678abeb3ae0e519fe
Jurek <jurek.szymanski@gmail.com>
2020-09-08 21:57:12 +0200

    Adding Beer Controller tests. Closes #4

--- src/main/java/prv/jws/microservices/beerservice/web/controller/impl/BeerControllerImpl.java
+++ src/main/java/prv/jws/microservices/beerservice/web/controller/impl/BeerControllerImpl.java
@@ -1,5 +1,7 @@
 package prv.jws.microservices.beerservice.web.controller.impl;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
@@ -9,23 +11,27 @@ import prv.jws.microservices.beerservice.web.controller.BeerController;
 import prv.jws.microservices.beerservice.web.model.BeerDto;
 
 @RestController
+@Slf4j
 public class BeerControllerImpl implements BeerController {
 
     @Override
     public ResponseEntity<BeerDto> getBeerById(final UUID beerId) {
         //TODO implement!
+        log.info("Getting by id {}", beerId);
         return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity saveNewBeer(final BeerDto beerDto) {
         //TODO implement
+        log.info("Posting beer with name {}", beerDto.getBeerName());
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @Override
     public ResponseEntity updateBeerById(final UUID beerId, final BeerDto beerDto) {
         //TODO implement
+        log.info("updating beer with id {}", beerId);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
---
+++ src/test/java/prv/jws/microservices/beerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,51 @@
+package prv.jws.microservices.beerservice.web.controller;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import prv.jws.microservices.beerservice.web.model.BeerDto;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void getBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveNewBeer() throws Exception {
+        BeerDto beerDto = BeerDto.builder().build();
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+        mockMvc.perform(post("/api/v1/beer/").contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void updateBeerById() throws Exception {
+        BeerDto beerDto = BeerDto.builder().build();
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+        mockMvc.perform(put("/api/v1/beer/"+UUID.randomUUID()).contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isNoContent());
+    }
+}
