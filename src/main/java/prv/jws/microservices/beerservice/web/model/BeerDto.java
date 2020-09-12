7e83f6f46ae6527e10eac42bf572e901e052a7b3
Jurek <jurek.szymanski@gmail.com>
2020-09-12 18:06:02 +0200

    Added Json formatting for timestamps (pattern) and price (String). Closes #13

--- src/main/java/prv/jws/microservices/beerservice/web/model/BeerDto.java
+++ src/main/java/prv/jws/microservices/beerservice/web/model/BeerDto.java
@@ -13,6 +13,9 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,8 +27,10 @@ public class BeerDto {
     private Integer version;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = Shape.STRING)
     private OffsetDateTime createdDate;
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
@@ -39,6 +44,7 @@ public class BeerDto {
 
     @NotNull
     @Positive
+    @JsonFormat(shape = Shape.STRING)
     private BigDecimal price;
 
     private Integer quantityOnHand;
