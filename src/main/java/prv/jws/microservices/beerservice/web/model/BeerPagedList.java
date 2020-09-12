b6c4381879cc0faab11c4435532b04cac7cc14d1
Jurek <jurek.szymanski@gmail.com>
2020-09-12 18:16:29 +0200

    Added @JsonCreator example for request with paged object. Closes #14

--- src/main/java/prv/jws/microservices/beerservice/web/model/BeerPagedList.java
+++ src/main/java/prv/jws/microservices/beerservice/web/model/BeerPagedList.java
@@ -2,12 +2,28 @@ package prv.jws.microservices.beerservice.web.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 
 public class BeerPagedList extends PageImpl<BeerDto> {
-    public BeerPagedList(final List<BeerDto> content, final Pageable pageable, final long total) {
-        super(content, pageable, total);
+
+    @JsonCreator(mode= Mode.PROPERTIES)
+    public BeerPagedList(
+            @JsonProperty("content") final List<BeerDto> content,
+            @JsonProperty("number") int number,
+            @JsonProperty("size") int size,
+            @JsonProperty("totalElements") Long totalElements,
+            @JsonProperty("pageable") JsonNode pageable,
+            @JsonProperty("last") boolean last,
+            @JsonProperty("totalPages") int totalPages,
+            @JsonProperty("sort") JsonNode sort,
+            @JsonProperty("first") boolean first,
+            @JsonProperty("numberOfElements") int numberOfElements) {
+        super(content, PageRequest.of(number, size), totalElements);
     }
 
     public BeerPagedList(final List<BeerDto> content) {
