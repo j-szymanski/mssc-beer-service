e1bbff906c84f77e12a343b54f8c768022130523
Jurek <jurek.szymanski@gmail.com>
2020-09-24 00:25:04 +0200

    Implementation of Validate order request Action with sending jms message to inventory service. Closes #9

--- src/main/java/prv/jws/beer/service/config/BreweryJarConfig.java
+++
@@ -1,9 +0,0 @@
-package prv.jws.beer.service.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages={"prv.jws.brewery"})
-public class BreweryJarConfig {
-}
