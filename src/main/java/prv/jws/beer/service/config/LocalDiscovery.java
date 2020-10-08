bd949fe0730832571be838188540262983aecfac
Jurek <jurek.szymanski@gmail.com>
2020-10-08 12:29:53 +0200

    Initial checkin with some debug for WORKING docker compose.yaml

--- src/main/java/prv/jws/beer/service/config/LocalDiscovery.java
+++ src/main/java/prv/jws/beer/service/config/LocalDiscovery.java
@@ -1,5 +1,8 @@
 package prv.jws.beer.service.config;
 
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -7,8 +10,15 @@ import org.springframework.context.annotation.Profile;
 /**
  * Created by Jerzy Szymanski on 04.10.2020 at 16:19
  */
+@Slf4j
 @Profile("local-discovery")
 @EnableDiscoveryClient
 @Configuration
 public class LocalDiscovery {
+
+    public LocalDiscovery(@Value("${eureka.client.serviceUrl.defaultZone}") String eurekaUrl,
+                          @Value("${sfg.brewery.inventory.beer-inventory-service-host}")String beerInventoryUrl) {
+        log.info("    >>>>    LOCAL DISCOVERY: eureka {}", eurekaUrl);
+        log.info("    >>>>    LOCAL DISCOVERY: Inventory {}", beerInventoryUrl);
+    }
 }
